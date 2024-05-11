7f87449823d1167085cbf1f5b111768de3c5921e
SANTHEESH S <150119668+Santheesh-s@users.noreply.github.com>
2024-05-11 14:23:55 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "if0_36525789";
+    $password = "santheesh";
     $dbname = "lenscraft";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
